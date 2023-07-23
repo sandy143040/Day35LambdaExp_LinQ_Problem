5fe906fca32132267652684b568f1d86d6c4e662
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-07-24 05:10:32 +0530

    [Sandeep] Add : Ratings greater then 3 by ProductId using LinQ

--- LambdaExp_Linq/LambdaExp_Linq/Management.cs
+++ LambdaExp_Linq/LambdaExp_Linq/Management.cs
@@ -25,18 +25,25 @@ namespace LambdaExp_Linq
                 Console.WriteLine(prod.ProductID + " " + prod.UserID + " " + prod.Rating + " " + prod.Review + "  " + prod.IsLike);
             }
         }
-        public void ProductRating(List<ProductReview> product)
+        public void ProductRating(List<ProductReview> products)
         {
-            var result = product.Where(x => x.Rating > 3 && (x.ProductID == 1 || x.ProductID == 4 || x.ProductID == 9));
-            Console.WriteLine("Using Lambda:");
-            Display(result);
-        }
-        public void Display(IEnumerable<ProductReview> result)
-        {
-            foreach (var prod in result)
+           // var result = products.Where(x => x.Rating > 3 && (x.ProductID == 1 || x.ProductID == 4 || x.ProductID == 9));
+            var record = from product in products where (product.ProductID == 1 || product.ProductID == 4 || product.ProductID == 9) && product.Rating > 3 select product;
+            Console.WriteLine("Using LinQ:");
+            foreach (var prod in record)
             {
                 Console.WriteLine(prod.ProductID + "  " + prod.UserID + "  " + prod.Rating + "  " + prod.Review + "  " + prod.IsLike);
+
             }
+            Console.WriteLine("-----------------------------");
+            // Display(result);
         }
+        //public void Display(IEnumerable<ProductReview> result)
+        //{
+        //    foreach (var prod in result)
+        //    {
+        //        Console.WriteLine(prod.ProductID + "  " + prod.UserID + "  " + prod.Rating + "  " + prod.Review + "  " + prod.IsLike);
+        //    }
+        //}
     }
 }
